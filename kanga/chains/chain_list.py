f63fcd34b4408d912154f1495a8236dd92288b49
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-08-08 10:25:50 -0400

    Finished getters for chain array

--- kanga/chains/chain_list.py
+++ kanga/chains/chain_list.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from pathlib import Path
+
 class ChainArray:
     def __init__(self, vals):
         self.reset(vals)
@@ -14,10 +16,38 @@ class ChainArray:
         not_converted = []
 
         for key in keys:
-            if key in ('accepted', 'target_val', 'sample', 'grad_val'):
-
-                # To do: read path.joinpath(key+'.csv') to array
+            if key in ('sample', 'target_val', 'grad_val', 'accepted'):
+                vals[key] = np.loadtxt(
+                    Path(path).joinpath(key+'.csv'), dtype=int if key == 'accepted' else dtype, delimiter=','
+                )
             else:
                 not_converted.append(key)
 
         return selfclass(vals), not_converted
+
+    def __repr__(self):
+        return f"Markov chain containing {len(self.vals['sample'])} samples."
+
+    def __len__(self):
+        return len(self.vals['sample'])
+
+    def get_sample(self, i):
+        return self.vals['sample'][:, i]
+
+    def get_samples(self):
+        return self.vals['sample']
+
+    def get_target_vals(self):
+        return self.vals['target_val']
+
+    def state(self, i=-1):
+        current = {}
+        for key, val in self.vals.items():
+            try:
+                current[key] = val[i]
+            except IndexError:
+                print(f'WARNING: chain does not have values for {key}.')
+                pass
+        return current
+
+# chain_array, _ = ChainArray.from_file('/Users/9tp/tmp/testing', keys=['sample'])
