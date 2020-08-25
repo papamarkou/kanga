c5238af34066e8e53a067bd907bb61b192cc3129
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-08-25 05:49:28 -0400

    Removed a redundant lambda function

--- kanga/stats/mean_array_summary.py
+++ kanga/stats/mean_array_summary.py
@@ -3,4 +3,4 @@ from operator import add
 
 # x is a numpy array of 3 dimensions, (chain, MC iteration, parameter)
 def mean_array_summary(x, g):
-    return reduce(add, map(lambda m: g(m), x))/len(x)
+    return reduce(add, map(g, x))/len(x)
