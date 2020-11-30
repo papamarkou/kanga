45b25a63cb489285825138ad6ee300f91ca164cd
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-11-30 05:39:43 +0200

    Enabled Google cloud startup script

---
+++ installation/gstartup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Start up script for setting up environment on Ubuntu 20.04 LTS
+
+METAUSER='theodore'
+BASEDIR="/home/$METAUSER"
+
+export DOWNLOADDIR="$BASEDIR/downloads"
+export CONDADIR="$BASEDIR/opt/continuum/miniconda/miniconda3"
+
+export CONDABIN="$CONDADIR/bin/conda"
+
+export CONDASCRIPT='Miniconda3-latest-Linux-x86_64.sh'
+export CONDAENV='kanga'
+
+sudo apt-get update
+
+sudo apt-get install tree
+
+su - $METAUSER -c "mkdir $DOWNLOADDIR"
+su - $METAUSER -c "cd $DOWNLOADDIR"
+su - $METAUSER -c "wget https://repo.anaconda.com/miniconda/$CONDASCRIPT"
+su - $METAUSER -c "chmod u+x $CONDASCRIPT"
+
+su - $METAUSER -c "$SHELL $CONDASCRIPT -b -p $CONDADIR"
+
+su - $METAUSER -c "$CONDABIN create -n $CONDAENV -y -c papamarkou -c conda-forge python=3.8 kanga"
+
+su - $METAUSER -c "$CONDABIN init $(basename $SHELL)"
+
+su - $METAUSER -c "$CONDABIN config --set auto_activate_base false"
