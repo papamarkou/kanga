7a9d99c31b9eb16058dc78fc807569e7528fad5a
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2020-11-30 14:05:35 +0200

    Cleaned up gcloud startup script

--- installation/gstartup.sh
+++ installation/gstartup.sh
@@ -5,7 +5,6 @@
 METAUSER='theodore'
 BASEDIR="/home/$METAUSER"
 
-export DOWNLOADDIR="$BASEDIR/downloads"
 export CONDADIR="$BASEDIR/opt/continuum/miniconda/miniconda3"
 
 export CONDABIN="$CONDADIR/bin/conda"
@@ -17,8 +16,6 @@ sudo apt-get update
 
 sudo apt-get install tree
 
-su - $METAUSER -c "mkdir $DOWNLOADDIR"
-su - $METAUSER -c "cd $DOWNLOADDIR"
 su - $METAUSER -c "wget https://repo.anaconda.com/miniconda/$CONDASCRIPT"
 su - $METAUSER -c "chmod u+x $CONDASCRIPT"
 
@@ -27,5 +24,6 @@ su - $METAUSER -c "$SHELL $CONDASCRIPT -b -p $CONDADIR"
 su - $METAUSER -c "$CONDABIN create -n $CONDAENV -y -c papamarkou -c conda-forge python=3.8 kanga"
 
 su - $METAUSER -c "$CONDABIN init $(basename $SHELL)"
-
 su - $METAUSER -c "$CONDABIN config --set auto_activate_base false"
+
+su - $METAUSER -c "rm $CONDASCRIPT"
